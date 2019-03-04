d4b338ad0460002bff42de7bc3bf726860eef1b5
binzhong <binz@mail.ustc.edu.cn>
2019-03-04 22:27:56 +0800

    fix some bugs

--- Generalized_Pooling.py
+++ Generalized_Pooling.py
@@ -1,20 +1,24 @@
 # -*- coding:utf-8 -*-
 
 import tensorflow as tf
+from tensorflow.contrib import layers
 
-
-def get_attention_weights(inputs, num_da, scope, scope_reuse=False):
+def get_attention_weights(inputs, num_da, scope, scope_reuse=False, regularizer=None):
     with tf.variable_scope(scope, reuse=scope_reuse):
         input_size = inputs.get_shape()[-1]
 
         W_1 = tf.get_variable("W_1", shape=[input_size, num_da],
-                              dtype=tf.float32, initializer=tf.truncated_normal_initializer())
+                              dtype=tf.float32, initializer=tf.truncated_normal_initializer(),
+                              regularizer=regularizer)
         b_1 = tf.get_variable("b_1", shape=[num_da],
-                              dtype=tf.float32, initializer=tf.constant_initializer(0.1))
+                              dtype=tf.float32, initializer=tf.constant_initializer(0.1),
+                              regularizer=regularizer)
         W_2 = tf.get_variable("W_2", shape=[num_da, input_size],
-                              dtype=tf.float32, initializer=tf.truncated_normal_initializer())
+                              dtype=tf.float32, initializer=tf.truncated_normal_initializer(),
+                              regularizer=regularizer)
         b_2 = tf.get_variable("b_2", shape=[input_size],
-                              dtype=tf.float32, initializer=tf.constant_initializer(0.1))
+                              dtype=tf.float32, initializer=tf.constant_initializer(0.1),
+                              regularizer=regularizer)
 
         temp = tf.nn.relu(tf.einsum('abc,cd->abd', inputs, W_1) + b_1)
         A = tf.einsum('abc,cd->abd', temp, W_2) + b_2
@@ -28,11 +32,13 @@ def get_masked_weights(inputs, seq_len, max_len):
     outputs = tf.nn.softmax(outputs, axis=1)
     return outputs
 
-def fc_layer(inputs, output_size, dropout_keep_prob, scope, scope_reuse=False):
+def fc_layer(inputs, output_size, dropout_keep_prob, scope, scope_reuse=False, regularizer=None):
     with tf.variable_scope(scope, reuse=scope_reuse):
         input_size = inputs.get_shape()[-1].value
-        W = tf.get_variable("W_fc", shape=[input_size, output_size], initializer=tf.orthogonal_initializer())
-        b = tf.get_variable("b_fc", shape=[output_size], initializer=tf.zeros_initializer())
+        W = tf.get_variable("W_fc", shape=[input_size, output_size], initializer=tf.orthogonal_initializer(),
+                            regularizer=regularizer)
+        b = tf.get_variable("b_fc", shape=[output_size], initializer=tf.zeros_initializer(),
+                            regularizer=regularizer)
         outputs = tf.nn.relu(tf.matmul(inputs, W) + b)
         outputs = tf.nn.dropout(outputs, keep_prob=dropout_keep_prob)
     return outputs
@@ -41,8 +47,9 @@ def fc_layer(inputs, output_size, dropout_keep_prob, scope, scope_reuse=False):
 class GeneralizedPooling(object):
     def __init__(self, max_len_left, max_len_right, vocab_size,
                 embedding_size, num_stack, rnn_size, num_heads, num_da,
-                 num_hidden, penalty_type=0, mu=1e-3, l2_reg_lambda=0.0):
+                 num_hidden, penalty_type=0, mu=1e-2, l2_reg_lambda=0.0):
 
+        regularizer = layers.l2_regularizer(l2_reg_lambda)
         # placeholder for input data
         self.input_left = tf.placeholder(tf.int32, shape=[None, max_len_left],
                                          name="input_left")
@@ -53,11 +60,11 @@ class GeneralizedPooling(object):
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         with tf.name_scope("embedding"):
-            W_weight = tf.get_variable(shape=[vocab_size, embedding_size],
+            W_weight = tf.get_variable(name='embedding_weights', shape=[vocab_size, embedding_size],
                                         dtype=tf.float32,
                                         initializer=tf.truncated_normal_initializer())
-            self.embedding_weight = tf.concat([tf.constant(tf.zeros([1, embedding_size])), W_weight[1:]],
-                                              axis=1, name='embedding_weights')
+            self.embedding_weight = tf.concat([tf.zeros([1, embedding_size]), W_weight[1:, :]],
+                                              axis=0)
 
             self.emb_left  = tf.nn.embedding_lookup(self.embedding_weight, self.input_left, name="emb_left")
             self.emb_right = tf.nn.embedding_lookup(self.embedding_weight, self.input_right, name="emb_right")
@@ -106,10 +113,10 @@ class GeneralizedPooling(object):
             W_1 = []
             for i in range(num_heads):
                 # consider mask the padding token
-                A_i_left, w_1  = get_attention_weights(self.H_left, num_da, "head_{}".format(i), scope_reuse=False)
+                A_i_left, w_1  = get_attention_weights(self.H_left, num_da, "head_{}".format(i), scope_reuse=False, regularizer=regularizer)
                 A_i_left = get_masked_weights(A_i_left, self.length_left, max_len_left)
 
-                A_i_right, _   = get_attention_weights(self.H_right, num_da, "head_{}".format(i), scope_reuse=True)
+                A_i_right, _   = get_attention_weights(self.H_right, num_da, "head_{}".format(i), scope_reuse=True, regularizer=regularizer)
                 A_i_right = get_masked_weights(A_i_right, self.length_right, max_len_right)
 
                 v_i_left  = tf.reduce_sum(tf.multiply(A_i_left, self.H_left), axis=1)
@@ -127,15 +134,16 @@ class GeneralizedPooling(object):
             if penalty_type == 0:
                 for i in range(num_heads):
                     for j in range(i+1, num_heads):
-                        self.penalty += tf.relu(1 - tf.square(tf.norm(W_1[i]-W_1[j], ord='fro', axis=[0,1])))
+                        self.penalty += tf.nn.relu(1 - tf.square(tf.norm(W_1[i]-W_1[j], ord='fro', axis=[0,1])))
 
         with tf.name_scope('mlp_layer'):
             self.V = tf.concat([self.V_left, self.V_right, tf.abs(self.V_left-self.V_right),
                                 tf.multiply(self.V_left, self.V_right)], axis=-1)
 
-            output = fc_layer(self.V, num_hidden, self.dropout_keep_prob, 'fc_1', scope_reuse=False)
+            output = fc_layer(self.V, num_hidden, self.dropout_keep_prob, 'fc_1', scope_reuse=False, regularizer=regularizer)
             # has a shortcut connection
-            self.full_put = fc_layer(tf.concat([self.V, output], axis=-1), num_hidden, self.dropout_keep_prob, 'fc_2', scope_reuse=False)
+            self.full_out = fc_layer(tf.concat([self.V, output], axis=-1), num_hidden, self.dropout_keep_prob, 'fc_2', scope_reuse=False,
+                                     regularizer=regularizer)
 
         with tf.name_scope("output"):
             W = tf.get_variable(
@@ -149,7 +157,7 @@ class GeneralizedPooling(object):
         # CalculateMean cross-entropy loss
         with tf.name_scope("loss"):
             losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
-            self.loss = tf.reduce_mean(losses) + mu * self.penalty
+            self.loss = tf.reduce_mean(losses) + mu * self.penalty + sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
 
         # Accuracy
         with tf.name_scope("accuracy"):
@@ -162,13 +170,5 @@ class GeneralizedPooling(object):
         x_sign = tf.sign(tf.abs(x))
         length = tf.reduce_sum(x_sign, axis=1)
         return tf.cast(length, tf.int32)
-
-
-if __name__ == '__main__':
-    a = tf.sequence_mask([1,3,4], 5)
-
-    with tf.Session() as sess:
-        a_ = sess.run(a)
-        print(a_)
 
 
